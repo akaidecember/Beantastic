37aeae125e1e7ce0f2d60560971eb603f0111d27
akaidecember <akaidecember@gmail.com>
2020-10-25 17:14:28 -0700

    Initial commit

---
+++ Beantastic/src/Core/Game.java
@@ -0,0 +1,50 @@
+package Core;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
+import ray.rage.*;
+import ray.rage.game.*;
+import ray.rage.rendersystem.*;
+import ray.rage.rendersystem.Renderable.*;
+import ray.rage.scene.*;
+import ray.rage.scene.Camera.Frustum.*;
+import ray.rage.scene.controllers.*;
+import ray.rml.*;
+import ray.rage.rendersystem.gl4.GL4RenderSystem;
+
+import ray.rage.rendersystem.states.*;
+import ray.rage.asset.texture.*;
+import ray.input.*;
+import ray.input.action.*;
+
+import ray.rage.rendersystem.shader.*;
+import ray.rage.util.*;
+import GameEngine.*;
+import net.java.games.input.Controller;
+
+public class Game extends VariableFrameRateGame{
+
+	@Override
+	protected void setupCameras(SceneManager arg0, RenderWindow arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setupScene(Engine arg0, SceneManager arg1) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void update(Engine arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
